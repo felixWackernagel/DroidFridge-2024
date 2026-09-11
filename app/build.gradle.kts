e5d8a335f34ae76b161aff701a0891fb8b938f8b
Felix Wackernagel <felix.wackernagel@exedio.com>
2026-09-11 10:16:16 +0200

    Release 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "de.wackernagel.droidfridge"
         minSdk = 29
         targetSdk = 37
-        versionCode = 3
+        versionCode = 4
         versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
